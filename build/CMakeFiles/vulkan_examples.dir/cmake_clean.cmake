a146d2dc37961c605410010700d2b6164a6a023a
Meleph <radionemu@konkuk.ac.kr>
2023-11-06 21:02:22 +0900

    Initial commit

---
+++ build/CMakeFiles/vulkan_examples.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkan_examples.dir/ex01.cpp.obj"
+  "CMakeFiles/vulkan_examples.dir/ex01.cpp.obj.d"
+  "libvulkan_examples.dll.a"
+  "vulkan_examples.exe"
+  "vulkan_examples.exe.manifest"
+  "vulkan_examples.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkan_examples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
